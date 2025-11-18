a33f53a0eeb69ccf0a06f63c4d6538b08192d565
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-19 00:44:55 +0500

    fixed layout

--- components/mainComponents/Header.tsx
+++ components/mainComponents/Header.tsx
@@ -11,25 +11,26 @@ const Header = () => {
     const [sideBar, setSideBar] = useState(false)
 
   return (
-    <header className='lg:px-30 md:px-20 sm:px-10 px-4 bg-white'>
+    <header className='xl:px-30 absolute w-full lg:px-20 md:px-10 px-4 bg-white'>
       <div className='flex justify-between items-center py-4'>
         <div>
             <Image src={"/logo.jpg"} alt='logo of shop' width={70} height={70} />
         </div>
+        <nav className='lg:flex hidden items-center justify-center'>
+          <ul className='flex gap-6 lg:gap-8 items-center pb-4'>
+          {navigation.map(item => (
+              <li key={item.title}><Link className='text-sm text-gray-700 hover:text-font whitespace-nowrap hover:underline duration-200' href={item.link}>{item.title}</Link></li>
+          ))}
+          </ul>
+        </nav>
         <div className='flex gap-5 items-center'>
             <Link href={"/cart"}><ShoppingBag /></Link>
-            {!sideBar && <Menu onClick={() => setSideBar(true)} className='inline-block md:hidden' />}
-            {sideBar && <X onClick={() => setSideBar(false)} className='inline-block md:hidden' />}
+            {!sideBar && <Menu onClick={() => setSideBar(true)} className='inline-block lg:hidden' />}
+            {sideBar && <X onClick={() => setSideBar(false)} className='inline-block lg:hidden' />}
         </div>
       </div>
-      <nav className='md:flex hidden items-center justify-center'>
-        <ul className='flex gap-6 lg:gap-8 items-center pb-4'>
-        {navigation.map(item => (
-            <li key={item.title}><Link className='text-sm text-gray-700 hover:text-font whitespace-nowrap hover:underline duration-200' href={item.link}>{item.title}</Link></li>
-        ))}
-        </ul>
-      </nav>
-      {sideBar && <nav className='flex md:hidden items-center justify-center'>
+      
+      {sideBar && <nav className='flex lg:hidden items-center justify-center'>
         <ul className='flex flex-col gap-8 items-center py-6'>
         {navigation.map(item => (
             <li key={item.title}><Link className='text-sm text-font' href={item.link}>{item.title}</Link></li>
--- components/mainComponents/Hero.tsx
+++ components/mainComponents/Hero.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
     <section>
-      
+      <Image src={"/hero.jpg"} alt='hero main image banner' width={1000} height={1000} className='w-full h-[120vh] object-cover object-bottom' />
+      <div className='absolute top-0'>
+
+      <h1>
+        Style Your Hair, Elevate Your Look
+      </h1>
+      <p>
+        Discover premium hair bows, clips, scrunchies, and headbands to match every mood and outfit.
+      </p>
+      </div>
     </section>
   )
 }

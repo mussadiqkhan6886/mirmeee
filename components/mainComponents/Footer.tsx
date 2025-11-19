6a2d4b9eb0b9e7c916c7d634c9c5f37a92ba9249
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-19 19:29:35 +0500

    done footer

--- components/mainComponents/Footer.tsx
+++ components/mainComponents/Footer.tsx
@@ -1,9 +1,21 @@
+import { Facebook, Instagram, TicketIcon } from 'lucide-react'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className='h-[300px] bg-medium text-light flex flex-col items-center justify-between pt-8'>
+      <div className='text-center'>
+      <h4 className='font-semibold text-3xl'>Follow Us</h4>
+      <p>Follow us for design ideas, exclusive offers, and new arrivals.</p>
+      </div>
+      <div className='flex gap-10'>
+        <Instagram />
+        <Facebook />
+        
+      </div>
+      <div className='text-sm mb-2'>
+        <p>&copy; {new Date().getFullYear() } Mirmee</p>
+      </div>
     </footer>
   )
 }
